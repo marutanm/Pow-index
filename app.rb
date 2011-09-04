7ddbe274bb8089ffbaf93af9c755bd22391c07c4
marutanm <marutanm@gmail.com>
2011-09-04 22:36:23 +0900

    Create simple index page

--- app.rb
+++ app.rb
@@ -1,3 +1,21 @@
+POW_PATH = "#{ENV['HOME']}/.pow"
+enable :inline_templates
+
 get '/' do
-  'hello world!'
+  @pows = Dir[POW_PATH + "/*"].map { |link| File.basename(link) }
+  haml :index
 end
+
+__END__
+
+@@ layout
+%html
+  %title pow index
+  = yield
+
+@@ index
+%ul
+  - @pows.each do |pow|
+    %li
+      %a{:href => 'http://' + pow + '.dev'} 
+        = pow
